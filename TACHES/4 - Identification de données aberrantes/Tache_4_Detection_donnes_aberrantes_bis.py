48b98c7b988a696a6b6238c43dc87c47942a5d36
Piasentin <beryl.piasentin@free.fr>
2020-04-02 23:40:43 +0200

    Correction de la variance (et fonctions d'Amélys mais sans fusionner)

--- TACHES/4 - Identification de données aberrantes/Tache_4_Detection_donnes_aberrantes_bis.py
+++ TACHES/4 - Identification de données aberrantes/Tache_4_Detection_donnes_aberrantes_bis.py
@@ -78,6 +78,11 @@ def test_Chauvenet(x,i):
     n = len(x) 
     x_barre = sum(x)/n
     var_x = (1/n)*sum(np.array(x)**2) - x_barre**2
+    
+    # Si la variance est nulle, tous les points sont égaux : aucun d'eux n'est aberrant.
+    if var_x == 0 :
+        return False
+        
     a = abs(x[i]-x_barre)/var_x**(0.5)
     n_a = (2*stat.norm.cdf(a,loc = 0,scale = 1)-1)
     if n_a > 0.5 :
@@ -104,17 +109,103 @@ def thompson(x,i,alpha=0.001):
     x_barre = sum(x)/n
     var_x = (1/n)*sum(np.array(x)**2) - x_barre**2
     sigma = var_x**(0.5)
-    #print("x_barre == ",x_barre)
-    #print("sigma == ", sigma)
     t_alpha = stat.t.ppf(alpha/2,n-1)
     seuil = t_alpha/((n**(0.5))*(n-2+t_alpha**2)**(0.5))
     gam = (x[i]-x_barre)/sigma 
-    #print(i , x[i],  seuil ,  gam)
     if gam > seuil :
         return True
     else :
         return False
         
+
+###############################################
+# Fonctions de supression de points aberrants #
+###############################################
+
+    
+def supprime(x,methode,sup_poids= True,poids=1/100):
+    """
+    Parcours toutes les valeurs de x afin de toutes les traiter.
+    La fonction supprime prend un vecteur x d'ordonnées de points, une methode de
+    detection des points aberrants, un booléen sup_poids égal à True si on veut supprimer
+    les points aberrants, et égal à False si on veut affecter le poids "poids" aux points
+    aberrants et un poids = 1 aux points considérés comme adaptés.
+    Elle renvoie une liste d'ordonnées ne contenant pas celles supprimées,
+    une liste de poids, ainsi qu'une liste des indices dans le vecteur des valeurs supprimées.
+    
+    type des entrees :
+        x : vecteur de float ou vecteur d'int
+        methode : fonction :vecteur de float ou vecteur d'int -> (float,float)
+        sup_poids : booleen
+        poids : float
+        
+    type des sorties : tuple (x_sup,v_poids,indices)
+        x_sup : vecteur de float ou vecteur d'int
+        v_poids : vecteur de float
+        indices : vecteur d'int
+    """
+    
+    n = len(x)
+    a,b = methode(x)
+    x_sup = list(x)
+    v_poids = [1]*n
+    indices = []
+    for i,e in enumerate(x):
+        if e <a or e>b:
+            indices.append(i)
+            if sup_poids:
+                x_sup[i] = None
+            else :
+                v_poids[i] = poids
+    
+    while None in x_sup:
+        x_sup.remove(None)
+    
+    return x_sup,v_poids,indices
+            
+
+def supprime_un(x,v_poids,i,methode,sup_poids= 2,poids=1/100): #COMMENTAIRE BERYL : PAS TOUCHEE, JE TE LE LAISSE AMELYS
+    """
+    Traite une valeur de x, donnée par l'indice i.
+    La fonction supprime prend un vecteur x d'ordonnées, le vecteur des poids associés,
+    un indice i de l'élément à supprimer, une méthode de détection des points aberrants, u
+    n entier sup_poids égal à 1 si on veut supprimer les points aberrants, 
+    égal à 2 si on veut garder la taille de x inchangée (None au lieu de points aberrants)
+    égal à 3 si on veut remplacer les points aberrants par les valeurs non aberrantes les plus proches (Méthode de Winsorising) :
+        - Affecte le quartile le plus proche pour la méthode interquartile
+        DECRIRE ICI CE QUI EST FAIT POUR LES AUTRES METHOOOOOOOOOOOOOOOODES
+    et égal à 0 si on veut affecter le poids "poids" aux points aberrants et un poids = 1 aux points normaux.
+    
+    type des entrees :
+        x : vecteur de float ou vecteur d'int
+        v_poids : vecteur de float
+        methode : fonction : vecteur de float ou vecteur d'int -> (float, float)
+        sup_poid : 0,1,2,3
+        poids : float
+        
+    type des sorties : couple (x_sup, v_poids)
+        x_sup : vecteur de float ou vecteur d'int
+        v_poids : vecteur de float
+    
+    """
+    
+    a,b = methode(x)
+    xk = x[i]
+    x_sup = list(x)
+    if xk <a or xk>b:
+        if sup_poids == 1:
+            x_sup.pop(i)
+            v_poids.pop(i)
+        elif sup_poids == 2:
+            x_sup[i] = None
+            v_poids[i] = None
+        elif sup_poids == 3:
+            x_sup[i] = a if xk<a else b
+        else:
+            v_poids[i] = poids
+
+    return x_sup,v_poids
+
 
 def supp_aberr(x,y,M=1) :
     """
@@ -204,8 +295,6 @@ if __name__ == "__main__" :
         Y = Y + yd
         
         i+=1 # On se décale d'un cran à droite
-        
-
 
     if M==1:
         lab = "chauvenet"
